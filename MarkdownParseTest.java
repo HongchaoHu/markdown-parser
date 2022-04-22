09f004b1998b6347f759f76d56d19caf17cf5bc3
JASON <hohu@ucsd.edu>
2022-04-22 13:42:26 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,8 +8,14 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void getLinks() {
+    public void getLinksTest1 () {
         String tester1 = "[link1](https://something.com) [link2](some-thing.html)";
         assertEquals("https://something.com some-thing.html", MarkdownParse.getLinks(tester1));
+    }
+
+    @Test
+    public void getLinksTest2() {
+        String tester2 = "[stuff] paragraph (page.com)";
+        assertEquals("page.com", MarkdownParse.getLinks(tester2));
     }
 }
