e432f244a9b0e20fb24b2d7101b2b029ae10f245
JASON <hohu@ucsd.edu>
2022-05-18 11:21:19 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -5,38 +5,51 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
-
 public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then read link upto next )
+
         int currentIndex = 0;
-        
-        while (currentIndex < markdown.length()) {
-            if (!markdown.substring(currentIndex,markdown.length()).contains("(")) {
-                return toReturn;
-            } 
-            else if (!markdown.substring(currentIndex,markdown.length()).contains("[")) {
-                return toReturn;
-            } 
-            if ((markdown.contains("[") || markdown.contains("]")) && (markdown.contains("(") || markdown.contains(")"))) {
+        if (!markdown.contains("[")){
+            return toReturn; 
+        } 
+
+        else if (!markdown.contains("[")){
+            return toReturn; 
+        }
+
+        else if (!markdown.contains("]")){
+            return toReturn; 
+        }
+
+        else if (!markdown.contains("(")){
+            return toReturn; 
+        }
+
+        else if (!markdown.contains(")")){
+            return toReturn; 
+        }
+
+        while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            int parenTotal = openParen + closeBracket; 
+            int bracketTotal = openBracket + closeBracket; 
+            if (parenTotal < bracketTotal){
+                return toReturn;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
-
-            System.out.println(currentIndex);
-            }    
-            
+            if (!markdown.substring(currentIndex,markdown.length()).contains("[")
+            || !markdown.substring(currentIndex,markdown.length()).contains("]")) {
+                return toReturn;
+            }
         }
-        //this is just a test file edit that needed for lab report 3, ignore this line!!
-
         return toReturn;
     }
-
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
